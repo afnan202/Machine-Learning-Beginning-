3e0c02c13aad4487879c881361446058854604fd
Afnan202 <afnanislam0202@gmail.com>
2024-10-20 11:06:02 +0600

    update-1

--- Code _ day _01.py
+++ Code _ day _01.py
@@ -1,4 +1,13 @@
+# In the first day of learning, I tried to learn pandas. Pandas mean Panel Data and Python
+# Data Analysis. Pandas mainly workks as a data filtering concepts where it can remove unnecessary null data or unnecessary data
+# which can help the dataset to be modified or to be cleared. I made to learn reading csv files, data analyze segments like 
+# finding min max, average or even correlation in between datasets etc.
+
 import numpy as np
 import pandas as pd
 import seaborn as sb
 import matplotlib as mp
+print(f"Pandas Version is {pd. __version__}") #printing version of panda
+print(f"Numpy Version is {np.__version__}") #printing version of numpy
+print(f"Seaborn Versions is {sb.__version__}") #printing version of seaborn
+print(f"Matplotlib Versions is {mp. __version__}") #printing version of matplotlib
